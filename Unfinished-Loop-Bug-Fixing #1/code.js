93f57cf9c4383f9e99ee87a25ef461bccb3c8e13
NosiphiwoB <biyelanosiphiwo@gmail.com>
2023-02-13 15:42:38 +0200

    unfinished loop bug fixing

---
+++ Unfinished-Loop-Bug-Fixing #1/code.js
@@ -0,0 +1,24 @@
+// unfinished  loop
+
+function createArray(number){
+    var newArray = [];
+    
+    for(var counter = 1; counter <= number;){
+      newArray.push(counter);
+    }
+    
+    return newArray;
+  }
+
+
+//   Solution
+
+function createArray(number){
+    var newArray = [];
+    
+    for(var counter = 1; counter <= number; counter++){
+      newArray.push(counter);
+    }
+    
+    return newArray;
+  }
